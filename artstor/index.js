1c0105588968d4e013a70b10c58c668adc81e86d
Kevin Wong <kkwokwai22@gmail.com>
2017-12-26 14:24:40 -0800

    style the main-part

--- artstor/index.js
+++ artstor/index.js
@@ -30,13 +30,19 @@ let generateContentPart = list => {
 let generateFirstPart = (idArr, wordArr, infoArr) => {
 	let firstPart = d.querySelector('.first-part');
 	for (let i = 0; i < idArr.length; i++) {
+		// genterate all the divs
 		let div = divGenerate();
 		let wordDiv = divGenerate();
 		let infoDiv = divGenerate();
+		let wrapDiv = divGenerate();
+		// setAttribute class and id
 		div.setAttribute('id', idArr[i]);
 		div.className = 'first-part-item';
+		wordDiv.className = 'custom-gray-wording';
+		infoDiv.className = 'custom';
 		wordDiv.textContent = wordArr[i];
 		infoDiv.textContent = infoArr[i];
+		// appendChild to div(first-part-item)
 		div.appendChild(wordDiv);
 		div.appendChild(infoDiv);
 		console.log(div);
